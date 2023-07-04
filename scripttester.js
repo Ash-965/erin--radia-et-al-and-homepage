f71dee410bae4c9297a651a946b79ea557de4d1c
LilLis2023 <138043031+LilLis2023@users.noreply.github.com>
2023-07-04 21:41:28 +0100

    Add files via upload

---
+++ scripttester.js
@@ -0,0 +1,83 @@
+function doSomething() {
+    alert("You are all signed up");
+} 
+
+
+// function myFunction() {
+//   let text;
+//   let person = prompt("Please enter your name:", "");
+//   if (person == null || person == "") {
+//     text = "User cancelled the prompt.";
+//   } else {
+//     text = "Hello " + person + "! How are you today?";
+//   }
+//   document.getElementById("demo").innerHTML = text;
+// }
+
+
+
+
+
+
+//concatenation this always uses a plus in the formula
+// const name = 'Lisa';
+// const greeting = 'Hi ' + name;
+
+// console.log(greeting);
+
+// interpolation is cleaner and better code practice but you can still use concat. You also need to be more aware of spaces if you use concat.
+
+// const name2 = 'Bob';
+// const city = 'Leeds';
+// const greeting2 = 'Hi ${name2} and I live in ${city}';
+
+// console.log(greeting2);
+
+// dont put numbers in quotes as they are not a string, they are a number
+//cntrl ck cntrl ku to comments the work
+
+const name3 = 'Lisa'; //string
+const age = 37; // number
+const futureJob = 'web developer'; //string
+const result = 'My name is ${name3}, I am ${age} years old and i will soon become a ${futureJob}';
+console.log(result)
+
+
+
+// let allergictoprawns = false;
+
+// if (allergictoprawns === true) {
+//     console.log('Guess we\'re having breadsticks!');
+// }
+// else {
+//     console.log('Bring on the shellfish')
+// }
+
+// let result1 = calcArea(10, 10);
+// console.log(result1)
+
+// function calcArea(width, height) {
+//     return width * height;
+
+// }
+
+
+// const name4 = 'Lorna';
+// const age4 = 29;
+// const futureJob4 = 'web dev'
+
+
+// console.log('Hello')
+
+// let name6 = prompt('What is your name?');
+// console.log('name6');
+
+// alert (`Hi ${name6}!`);
+
+// //use back ticks `` not single quotes for this (above)''
+
+// function pressAlert() {
+//     console.log('You clicked the button!');
+// }
+
+// addEventListener('click', pressAlert);
