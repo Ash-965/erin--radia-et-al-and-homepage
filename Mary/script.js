b338a4d7587be4e6479e7eee76f2d2df10d2307a
chloeallsopp <133899743+chloeallsopp@users.noreply.github.com>
2023-07-05 17:42:31 +0100

    Add files via upload

---
+++ Mary/script.js
@@ -0,0 +1,10 @@
+
+function showSuccessMessage() {
+    var form = document.getElementById('subscribeForm');
+    form.style.display = 'none';
+    
+    var successMessage = document.getElementById('successMessage');
+    successMessage.style.display = 'block';
+  }
+  
+
